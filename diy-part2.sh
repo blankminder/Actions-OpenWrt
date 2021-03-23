c26745559cb8e70e29209d5936b2bad484ea7ca9
blankminder <35001243+blankminder@users.noreply.github.com>
2021-03-23 17:29:55 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,6 +9,6 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
-
+git clone https://github.com/OpenWrt-Actions/OpenWrt-Packages
 # Modify default IP
-#sed -i 's/192.168.1.1/10.10.10.250/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.10.10.250/g' package/base-files/files/bin/config_generate
